8794707f40dc668b8359deb0ffcf5e92aa9b49d9
khalafkristoffer <khalafkristoffer@gmail.com>
2025-01-29 21:13:52 +0100

    Initalized backend in java spring using maven, frontend in react

---
+++ backend/src/main/java/backend_lf/BackendLfApplication.java
@@ -0,0 +1,13 @@
+package backend_lf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLfApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/backend_lf/BackendLfApplicationTests.java
@@ -0,0 +1,13 @@
+package backend_lf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
